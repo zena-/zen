6413fb1bfb842d2a3df61efa85dfea591c236fa2
Zena.e <zena.erhabor@barreldev.com>
2022-04-08 19:31:03 -0400

    fix

--- pages/index.js
+++ pages/index.js
@@ -216,7 +216,7 @@ export default function Home() {
             </section>
 
             <section className="late blog">
-              <p className="text-center sor">&#34;sorry I'm late, I didn't really want to&nbsp;come&#34;</p>
+              <p className="text-center sor">&#34;sorry I&#39;m late, I didn&#39;t really want to&nbsp;come&#34;</p>
               <div className="row text-center">
                 <div className="col-md col-sm-12" id="zenDou">
                   <div className="zenDou" title="so aloof | insta illustration by me"></div>
@@ -242,7 +242,7 @@ export default function Home() {
             </section>
 
             <section className="blog pinkLine">
-              <p className="text-center sometimes" id="pinkLineP">&#34;as for me, I'm trying to stay timeless in the era of constant&nbsp;change&#34;</p>
+              <p className="text-center sometimes" id="pinkLineP">&#34;as for me, I&#39;m trying to stay timeless in the era of constant&nbsp;change&#34;</p>
                 <div className="col-md text-center">
                   <div className="nb" id="zenNB">
                     <div className="zenNB anim" data-delay=".1s" title="so aloof | new balance illustration by me">
@@ -254,7 +254,7 @@ export default function Home() {
                    <div className="text-center hearts" id="pills-tab" role="tablist">
                     <div className="nav-item heart mx-auto">
                       <a className="nav-link deep" id="skincare-tab" data-toggle="pill" href="#skincare" role="tab" aria-controls="skincare" aria-selected="false">
-                        <span className="heartBtn">i'm a button</span>
+                        <span className="heartBtn">i&#39;m a button</span>
                       </a>
                     </div>
                   </div>
@@ -268,7 +268,7 @@ export default function Home() {
                     </div>
                     <div className="grid-item item border">
                         <h2 className="boxes text-center">Kypris</h2>
-                        <a target="_blank" rel="nofollow" href="https://www.amazon.com/gp/product/B01LWQMXG7/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B01LWQMXG7&linkCode=as2&tag=zena09-20&linkId=8ae480a1de620d996d93bf40c45dcc88">
+                        <a target="_blank" rel="noreferrer" href="https://www.amazon.com/gp/product/B01LWQMXG7/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B01LWQMXG7&linkCode=as2&tag=zena09-20&linkId=8ae480a1de620d996d93bf40c45dcc88">
                           <img className="anim" data-delay=".1s" alt="Kypris So Aloof natural | illustration by me" src="/kypris.jpg"/>
                         </a>
                     </div>
@@ -277,13 +277,13 @@ export default function Home() {
                     </div>
                     <div className="grid-item item border">
                       <h2 className="boxes text-center">Herbivore Botanicals</h2>
-                      <a target="_blank" rel="nofollow" href="https://www.amazon.com/gp/product/B07Q7YS8SY/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07Q7YS8SY&linkCode=as2&tag=zena09-20&linkId=5a8ef751f6f6d1e2794420edb5bb352a">
+                      <a target="_blank" rel="noreferrer" href="https://www.amazon.com/gp/product/B07Q7YS8SY/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07Q7YS8SY&linkCode=as2&tag=zena09-20&linkId=5a8ef751f6f6d1e2794420edb5bb352a">
                         <img className="margintop35 anim" data-delay=".1s" alt="Herbivore Botanicals So Aloof natural | illustration by me" src="/drawHerb.jpg"/>
                       </a>
                     </div>
                     <div className="grid-item item border">
-                      <h2 className="boxes text-center">Dr. Bronner's</h2>
-                      <a target="_blank" rel="nofollow" href="https://www.amazon.com/gp/product/B001ET725W/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B001ET725W&linkCode=as2&tag=zena09-20&linkId=04ab5ac72eb25088bd6b5b5dfd8b7da7">
+                      <h2 className="boxes text-center">Dr. Bronner&#39;s</h2>
+                      <a target="_blank" rel="noreferrer" href="https://www.amazon.com/gp/product/B001ET725W/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B001ET725W&linkCode=as2&tag=zena09-20&linkId=04ab5ac72eb25088bd6b5b5dfd8b7da7">
                         <img className="anim" data-delay=".1s" src="/drawDrB.jpg" alt="Dr. Bronner's So Aloof natural | illustration by me" />
                       </a>
                     </div>
@@ -292,13 +292,13 @@ export default function Home() {
                     </div>
                     <div className="grid-item item border" id="zenNoto">
                         <h2 className="boxes text-center">NOTO Botanics</h2>
-                        <a target="_blank" rel="nofollow" className="" href="https://www.amazon.com/gp/product/B07FJLZTYW/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07FJLZTYW&linkCode=as2&tag=zena09-20&linkId=8f352530c302a24b325ca6112a24b31e">
+                        <a target="_blank" rel="noreferrer" className="" href="https://www.amazon.com/gp/product/B07FJLZTYW/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07FJLZTYW&linkCode=as2&tag=zena09-20&linkId=8f352530c302a24b325ca6112a24b31e">
                           <img className="anim" data-delay=".2s" id="noto" alt="NOTO So Aloof natural | illustration by me" src="/noto.jpg"/>
                         </a>
                     </div>
                     <div className="grid-item item border">
                         <h2 className="boxes text-center">Aradé Beauty</h2>
-                        <a target="_blank" rel="nofollow" href="https://www.amazon.com/gp/product/B00VFFEAT6/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00VFFEAT6&linkCode=as2&tag=zena09-20&linkId=35ee06a8e2b63564562d886e6c0821a6">
+                        <a target="_blank" rel="noreferrer" href="https://www.amazon.com/gp/product/B00VFFEAT6/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00VFFEAT6&linkCode=as2&tag=zena09-20&linkId=35ee06a8e2b63564562d886e6c0821a6">
                           <img className="anim" data-delay=".2s" alt="Arade So Aloof natural | illustration by me" src="/drawArade.jpg"/>
                         </a>
                     </div>
@@ -307,13 +307,13 @@ export default function Home() {
                     </div>
                     <div className="grid-item item border">
                       <h2 className="boxes text-center">Rosehip oil</h2>
-                      <a target="_blank" rel="nofollow" href="https://www.amazon.com/gp/product/B01LM282RW/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B01LM282RW&linkCode=as2&tag=zena09-20&linkId=9741b547006ca82c29b4f65414615af9">
+                      <a target="_blank" rel="noreferrer" href="https://www.amazon.com/gp/product/B01LM282RW/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B01LM282RW&linkCode=as2&tag=zena09-20&linkId=9741b547006ca82c29b4f65414615af9">
                         <img className="anim" data-delay=".3s" alt="Rosehip oil So Aloof natural | illustration by me" src="/drawRose.jpg"/>
                       </a>
                     </div>
                     <div className="grid-item item border">
                       <h2 className="boxes text-center">LAPCOS</h2>
-                      <a target="_blank" rel="nofollow" href="https://www.amazon.com/gp/product/B07J4X9C51/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07J4X9C51&linkCode=as2&tag=zena09-20&linkId=9503a24b4880e2135f9663fcc30e2d0a">
+                      <a target="_blank" rel="noreferrer" href="https://www.amazon.com/gp/product/B07J4X9C51/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07J4X9C51&linkCode=as2&tag=zena09-20&linkId=9503a24b4880e2135f9663fcc30e2d0a">
                         <img className="margintop35 anim" data-delay=".3s" src="/lapcos.jpg" />
                       </a>
                     </div>
@@ -322,13 +322,13 @@ export default function Home() {
                     </div>
                     <div className="grid-item item border">
                       <h2 className="boxes text-center">iHeart Nature</h2>
-                      <a target="_blank" rel="nofollow" href="https://www.amazon.com/gp/product/B07JF7YBZK/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07JF7YBZK&linkCode=as2&tag=zena09-20&linkId=12e90add98aceea4ac9e07d6cabc56d9">
+                      <a target="_blank" rel="noreferrer" href="https://www.amazon.com/gp/product/B07JF7YBZK/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07JF7YBZK&linkCode=as2&tag=zena09-20&linkId=12e90add98aceea4ac9e07d6cabc56d9">
                         <img className="anim" data-delay=".3s" src="/iheart.jpg" />
                       </a>
                     </div>
                     <div className="grid-item item border">
                       <h2 className="boxes text-center">Pure Biology</h2>
-                      <a target="_blank" rel="nofollow" href="https://www.amazon.com/gp/product/B07P7Y5BGF/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07P7Y5BGF&linkCode=as2&tag=zena09-20&linkId=b34361134793173ee66bfebfcb15db88">
+                      <a target="_blank" rel="noreferrer" href="https://www.amazon.com/gp/product/B07P7Y5BGF/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07P7Y5BGF&linkCode=as2&tag=zena09-20&linkId=b34361134793173ee66bfebfcb15db88">
                         <img className="anim" data-delay=".4s" src="/pureB.jpg" />
                       </a>
                     </div>
@@ -337,19 +337,19 @@ export default function Home() {
                     </div>
                     <div className="grid-item item border">
                       <h2 className="boxes text-center">Rael</h2>
-                      <a target="_blank" rel="nofollow" href="https://www.amazon.com/gp/product/B0773HT3FT/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B0773HT3FT&linkCode=as2&tag=zena09-20&linkId=b785ed89a399369fd1e2efdbe76f402c">
+                      <a target="_blank" rel="noreferrer" href="https://www.amazon.com/gp/product/B0773HT3FT/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B0773HT3FT&linkCode=as2&tag=zena09-20&linkId=b785ed89a399369fd1e2efdbe76f402c">
                         <img className="anim" data-delay=".4s" src="/rael.jpg" />
                       </a>
                     </div>
                     <div className="grid-item item border">
                       <h2 className="boxes text-center">Ballon Blanc</h2>
-                      <a target="_blank" rel="nofollow" href="https://www.amazon.com/gp/product/B07Q4NPX1C/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07Q4NPX1C&linkCode=as2&tag=zena09-20&linkId=c016e16ae499abd2d00de7bba3a388a6">
+                      <a target="_blank" rel="noreferrer" href="https://www.amazon.com/gp/product/B07Q4NPX1C/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07Q4NPX1C&linkCode=as2&tag=zena09-20&linkId=c016e16ae499abd2d00de7bba3a388a6">
                         <img className="anim" data-delay=".4s" id="ballon" src="/ballon.jpg" />
                       </a>
                     </div>
                     <div className="grid-item item border">
                       <h2 className="boxes text-center">Andalou Naturals</h2>
-                      <a target="_blank" rel="nofollow" href="https://www.amazon.com/gp/product/B00854KDYQ/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00854KDYQ&linkCode=as2&tag=zena09-20&linkId=f59ea620bc4d544e507dbece038b1ef3">
+                      <a target="_blank" rel="noreferrer" href="https://www.amazon.com/gp/product/B00854KDYQ/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00854KDYQ&linkCode=as2&tag=zena09-20&linkId=f59ea620bc4d544e507dbece038b1ef3">
                         <img className="anim" data-delay=".5s" src="/andalou.jpg" />
                       </a>
                     </div>
